2e9cdd2cd3d12194bedcc3d1221322e6ab326017
Luis <112443531+LuisS1991@users.noreply.github.com>
2024-01-29 15:06:22 -0300

    Update PreLoad.java

--- src/PreLoad.java
+++ src/PreLoad.java
@@ -43,7 +43,7 @@ public class PreLoad extends JFrame {
 	 * Create the frame.
 	 */
 	public PreLoad() {
-		setIconImage(Toolkit.getDefaultToolkit().getImage(PreLoad.class.getResource("/img/bg.png")));
+		setIconImage(Toolkit.getDefaultToolkit().getImage(PreLoad.class.getResource("/imagenes/casaIcon.png")));
 		setTitle("Sistema Gestión de Taller");
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -108,13 +108,13 @@ public class PreLoad extends JFrame {
 
 		public void paint(Graphics grafico) {
 			// Se selecciona la imagen que tenemos en el paquete de la //ruta del programa
-			ImageIcon Img = new ImageIcon(getClass().getResource("/img/bg1.png"));
+			ImageIcon Img = new ImageIcon(getClass().getResource("/imagenes/casaIcon.png"));
 			// se dibuja la imagen que tenemos en el paquete Images //dentro de un panel
 			grafico.drawImage(Img.getImage(), 0, 0, 512, 512, null);
 			grafico.setColor(Color.darkGray);// color de letra
 			grafico.setFont(new Font(Font.MONOSPACED, Font.BOLD, 35));// fuente y tamaño
-			grafico.drawString("taller Pepito", 510, 150);// texto a escribir con las cordenasdas en x y
-			grafico.drawString("Sistema de Gestiòn de Repuestos", 510, 230);
+			grafico.drawString("Inmobiliaria ITS", 510, 150);// texto a escribir con las cordenasdas en x y
+			grafico.drawString("Sistema de Gestiòn de Inmuebles", 510, 230);
 			setOpaque(false);
 			super.paintComponent(grafico);
 		}
